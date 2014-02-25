c171d0446abcb503b25705b40500da6219c0d64f
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2014-02-25 15:48:58 +0200

    Added simpleTest method with simple return values

--- cors-tester.js
+++ cors-tester.js
@@ -1,59 +1,71 @@
-module.exports.runOnce = function(configParam, callbackParam) {
-  var parameterHandling = require('./lib/parameterHandling');
+var returnObjectLib = require('./lib/returnObject')
+  , parameterHandling = require('./lib/parameterHandling');
+
+module.exports.simpleTest = function(configParam, callbackParam) {
   var config = parameterHandling.getDefaultedConfig(configParam);
-  var callback = parameterHandling.getDefaultedCallback(callbackParam);
+  var callback = parameterHandling.getCallback(callbackParam);
+  makeRequest(configParam, function(retval) {
+    var simpleRetVal = returnObjectLib.convertToSimpleReturnValue(retval);
+    callbackParam(simpleRetVal);
+  });
+};
+
+module.exports.makeCorsRequest = function(configParam, callbackParam) {
+  var config = parameterHandling.getDefaultedConfig(configParam);
+  var callback = parameterHandling.getCallback(callbackParam);
+  makeRequest(config, callback);
+};
+
+
+function makeRequest(config, callback) {
   var phantom = require('node-phantom-simple');
   var params = {
     phantomPath: require('phantomjs').path
   };
   phantom.create(create, params);
+
   function create(err, ph) {
     if (err) {
-      throw new Error(err);
-    }
-    var singleTest = initialize(ph, config);
-    singleTest(function(networkEvents) {
-      ph.exit();
-      var returnObject = require('./lib/returnObject').getReturnObjectFromXhrNetworkEvents(config.url, networkEvents);
-      callback(returnObject);
-    });
-  } 
-}
-
-function initialize(ph, config) {
-
-  function singleTest(callback) {
-    var serverHost = 'http://localhost:' + config.port;
-    var url = serverHost + '/?url=' + config.url + '&method=' + config.method;
-    var server = require('./lib/requesterServer').createServer(config.port);
-    var networkEvents = {
-      requests: [],
-      responses: [],
-      errors: [],
-      timeouts: []
-    };
-    ph.createPage(openPage);
-
-    function openPage(err, page) {
-      page.open(url);
-
-      page.onResourceError = function(resourceError) {
-        networkEvents.errors.push(resourceError);
-      };
-      page.onResourceRequested = function(requestData, networkRequest) {
-        networkEvents.requests.push(requestData);
-      };
-
-      page.onResourceReceived = function(response) {
-        networkEvents.responses.push(response);
-      };
-
-      page.onLoadFinished = function(status) {
-        server.close();
-        callback(networkEvents);
-      };
+      callback(err);
+    } else {
+      singleTest(ph, config, function(networkEvents) {
+        ph.exit();
+        var returnObject = returnObjectLib.getReturnObjectFromXhrNetworkEvents(config.url, networkEvents);
+        callback(returnObject);
+      });
     }
   }
+}
 
-  return singleTest;
+function singleTest(ph, config, callback) {
+  var serverHost = 'http://localhost:' + config.port;
+  var url = serverHost + '/?url=' + config.url + '&method=' + config.method;
+  var server = require('./lib/requesterServer').createServer(config.port);
+  var networkEvents = {
+    requests: [],
+    responses: [],
+    errors: [],
+    timeouts: []
+  };
+  ph.createPage(openPage);
+
+  function openPage(err, page) {
+    page.open(url);
+
+    page.onResourceError = function(resourceError) {
+      networkEvents.errors.push(resourceError);
+    };
+    page.onResourceRequested = function(requestData, networkRequest) {
+      networkEvents.requests.push(requestData);
+    };
+
+    page.onResourceReceived = function(response) {
+      networkEvents.responses.push(response);
+    };
+
+    page.onLoadFinished = function(status) {
+      server.close();
+      callback(networkEvents);
+    };
+  }
 }
--- test/test.js
+++ test/test.js
@@ -5,11 +5,26 @@ var config = {
   timeout: 10000
 };
 
+config.corsEnabledURL = 'http://localhost:' + config.corsEnabledServerPort + '/';
+config.corsDisabledURL = 'http://localhost:' + config.corsDisabledServerPort + '/';
+
 var chai = require('chai')
   , should = chai.should()
   , expect = chai.expect
   , serverApp = require('./test_server')
-  , cors_tester_lib = require('./../cors-tester');
+  , corsTester = require('./../cors-tester');
+
+function createCorsEnabledServer(allowedOrigin) {
+  return serverApp
+      .createCorsEnabledServer(allowedOrigin)
+      .listen(config.corsEnabledServerPort);
+}
+
+function createCorsDisabledServer() {
+  return serverApp
+      .createCorsDisabledServer()
+      .listen(config.corsDisabledServerPort);
+}
 
 describe('CORS-tester', function() {
   this.timeout(config.timeout);
@@ -17,49 +32,48 @@ describe('CORS-tester', function() {
   describe('config', function() {
     describe('invalids', function() {
       it('should throw Error if no parameters are given', function() {
-        expect(cors_tester_lib.runOnce).to.throw(Error);
+        expect(corsTester.makeCorsRequest).to.throw(Error);
       });
 
       it('should throw TypeError if the first parameter is an int', function() {
         var fn = function() {
-          cors_tester_lib.runOnce(1);
+          corsTester.makeCorsRequest(1);
         };
         expect(fn).to.throw(TypeError);
       });
 
       it('should throw TypeError if config parameter is a function', function() {
         var fn = function() {
-          cors_tester_lib.runOnce(function(){});
+          corsTester.makeCorsRequest(function(){});
         };
         expect(fn).to.throw(TypeError);
       });
 
       it('should throw TypeError if callback parameter is not a function', function() {
         var fn = function() {
-          cors_tester_lib.runOnce('http://localhost:9000', 'http://localhost:9000');
+          corsTester.makeCorsRequest('http://localhost:9000', 'http://localhost:9000');
         };
         expect(fn).to.throw(TypeError);
       });
 
       it('should throw Error if config object doesnt have a url', function() {
         var fn = function() {
-          cors_tester_lib.runOnce({method: 'GET', port: 4004}, function() {});
+          corsTester.makeCorsRequest({method: 'GET', port: 4004}, function() {});
         };
         expect(fn).to.throw(Error);
       });
 
       it('should return an error if the url is malformed', function(done) {
-        cors_tester_lib.runOnce('http://localhost:999999999', function(retval) {
-          should.not.exist(retval.error.errorCode);
-          retval.error.errorString.should.equal('No XHR requests made. This is most likely due to malformed URL');
+        corsTester.makeCorsRequest('http://localhost:999999999', function(returnValue) {
+          returnValue.error.errorString.should.equal('No XHR requests made. This is most likely due to malformed URL');
           done();
         });
       });
 
       it('should return an error if the host doesnt exist', function(done) {
-        cors_tester_lib.runOnce('http://l:9999/?id=ddd', function(retval) {
-          retval.error.errorCode.should.equal(3);
-          retval.error.errorString.should.equal('Host l not found');
+        corsTester.makeCorsRequest('http://l:9999/?id=ddd', function(returnValue) {
+          returnValue.error.errorCode.should.equal(3);
+          returnValue.error.errorString.should.equal('Host l not found');
           done();
         });
       });
@@ -70,12 +84,9 @@ describe('CORS-tester', function() {
 
   describe('cors disabled', function() {
     var corsDisabledServer;
-    var corsDisabledURL = 'http://localhost:' + config.corsDisabledServerPort + '/';
 
     before(function() {
-      corsDisabledServer = serverApp
-        .createCorsDisabledServer()
-        .listen(config.corsDisabledServerPort);
+      corsDisabledServer = createCorsDisabledServer();
     });
 
     after(function() {
@@ -85,12 +96,12 @@ describe('CORS-tester', function() {
     it('should fail cors requests', function(done) {
       var testConfig = {
         port: config.corsRequesterPort,
-        url: corsDisabledURL,
+        url: config.corsDisabledURL,
         method: 'GET'
       };
-      cors_tester_lib.runOnce(testConfig, function(retval) {
-        retval.error.errorString.should.equal('Operation canceled');
-        retval.error.errorCode.should.equal(5);
+      corsTester.makeCorsRequest(testConfig, function(returnValue) {
+        returnValue.error.errorString.should.equal('Operation canceled');
+        returnValue.error.errorCode.should.equal(5);
         done();
       });
     });
@@ -99,13 +110,9 @@ describe('CORS-tester', function() {
 
   describe('cors enabled with a *', function() {
     var corsEnabledServer;
-    var corsEnabledURL = 'http://localhost:' + config.corsEnabledServerPort + '/';
-    var allowedOrigin = '*';
 
     before(function() {
-      corsEnabledServer = serverApp
-        .createCorsEnabledServer(allowedOrigin)
-        .listen(config.corsEnabledServerPort);
+      corsEnabledServer = createCorsEnabledServer('*');
     });
 
     after(function() {
@@ -115,11 +122,12 @@ describe('CORS-tester', function() {
     it('should work with only an url given', function(done) {
       var testConfig = {
         port: config.corsRequesterPort,
-        url: corsEnabledURL,
+        url: config.corsEnabledURL,
         method: 'GET'
       };
-      cors_tester_lib.runOnce(corsEnabledURL, function(retval) {
-        retval.success.should.equal('Success');
+      corsTester.makeCorsRequest(config.corsEnabledURL, function(returnValue) {
+        returnValue.response.status.should.equal(200);
+        returnValue.success.should.equal('Success');
         done();
       });
     });
@@ -127,11 +135,12 @@ describe('CORS-tester', function() {
     it('should pass cors requests', function(done) {
       var testConfig = {
         port: config.corsRequesterPort,
-        url: corsEnabledURL,
+        url: config.corsEnabledURL,
         method: 'GET'
       };
-      cors_tester_lib.runOnce(testConfig, function(retval) {
-        retval.success.should.equal('Success');
+      corsTester.makeCorsRequest(testConfig, function(returnValue) {
+        returnValue.response.status.should.equal(200);
+        returnValue.success.should.equal('Success');
         done();
       });
     });
@@ -139,12 +148,14 @@ describe('CORS-tester', function() {
     it('should return 404 if xhr returns 404', function(done) {
       var testConfig = {
         port: config.corsRequesterPort,
-        url: corsEnabledURL + 'notfound404',
+        url: config.corsEnabledURL + 'notfound404',
         method: 'GET'
       };
-      cors_tester_lib.runOnce(testConfig, function(retval) {
-        retval.error.errorCode.should.equal(203);
-        retval.error.errorString.should.equal('Error downloading http://localhost:4003/notfound404 - server replied: Not Found');
+      corsTester.makeCorsRequest(testConfig, function(returnValue) {
+        returnValue.response.status.should.equal(404);
+        returnValue.error.errorString.should.equal(
+          'Error downloading http://localhost:' + config.corsEnabledServerPort + '/notfound404 - server replied: Not Found'
+        );
         done();
       });
     });
@@ -152,25 +163,24 @@ describe('CORS-tester', function() {
     it('should pass the correct http method', function(done) {
       var testConfig = {
         port: config.corsRequesterPort,
-        url: corsEnabledURL + 'post',
+        url: config.corsEnabledURL + 'post',
         method: 'POST'
       };
-      cors_tester_lib.runOnce(testConfig, function(retval) {
-        retval.success.should.equal('Success');
+      corsTester.makeCorsRequest(testConfig, function(returnValue) {
+        returnValue.response.status.should.equal(200);
+        returnValue.success.should.equal('Success');
         done();
       });
     });
+
   });
 
   describe('cors enabled without a wilcard', function() {
     var corsEnabledServer;
-    var corsEnabledURL = 'http://localhost:' + config.corsEnabledServerPort + '/';
     var allowedOrigin = 'http://localhost:' + config.corsRequesterPort;
 
     before(function() {
-      corsEnabledServer = serverApp
-        .createCorsEnabledServer(allowedOrigin)
-        .listen(config.corsEnabledServerPort);
+      corsEnabledServer = createCorsEnabledServer(allowedOrigin);
     });
 
     after(function() {
@@ -180,11 +190,11 @@ describe('CORS-tester', function() {
     it('should pass cors requests from allowedOrigin', function(done) {
       var testConfig = {
         port: config.corsRequesterPort,
-        url: corsEnabledURL,
+        url: config.corsEnabledURL,
         method: 'GET'
       };
-      cors_tester_lib.runOnce(testConfig, function(retval) {
-        retval.success.should.equal('Success');
+      corsTester.makeCorsRequest(testConfig, function(returnValue) {
+        returnValue.success.should.equal('Success');
         done();
       });
     });
@@ -192,16 +202,66 @@ describe('CORS-tester', function() {
     it('should fail cors requests from non allowedOrigin', function(done) {
       var testConfig = {
         port: config.corsRequesterPort+1,
-        url: corsEnabledURL,
+        url: config.corsEnabledURL,
         method: 'GET'
       };
-      cors_tester_lib.runOnce(testConfig, function(retval) {
-        retval.error.errorString.should.equal('Operation canceled');
-        retval.error.errorCode.should.equal(5);
+      corsTester.makeCorsRequest(testConfig, function(returnValue) {
+        returnValue.error.errorString.should.equal('Operation canceled');
+        returnValue.error.errorCode.should.equal(5);
+        done();
+      });
+    });
+  });
+
+  describe('simpleTest', function() {
+    var corsEnabledServer;
+    var allowedOrigin = 'http://localhost:' + config.corsRequesterPort;
+
+    before(function() {
+      corsEnabledServer = createCorsEnabledServer(allowedOrigin);
+    });
+
+    after(function() {
+      corsEnabledServer.close();
+    });
+
+    it('should return success when cors passed', function(done) {
+      var testConfig = {
+        port: config.corsRequesterPort,
+        url: config.corsEnabledURL,
+        method: 'GET'
+      };
+      corsTester.simpleTest(testConfig, function(returnValue) {
+        returnValue.should.equal('Success!');
         done();
       });
     });
 
+    it('should fail cors requests from non allowedOrigin', function(done) {
+      var testConfig = {
+        port: config.corsRequesterPort+1,
+        url: config.corsEnabledURL,
+        method: 'GET'
+      };
+      corsTester.simpleTest(testConfig, function(returnValue) {
+        returnValue.should.equal('Cors not allowed!');
+        done();
+      });
+    });
+
+    it('should return 404 message if not found', function(done) {
+      var testConfig = {
+        port: config.corsRequesterPort,
+        url: config.corsEnabledURL + 'notfound404',
+        method: 'GET'
+      };
+      corsTester.simpleTest(testConfig, function(returnValue) {
+        returnValue.should.equal(
+          'Error downloading http://localhost:' + config.corsEnabledServerPort + '/notfound404 - server replied: Not Found'
+        );
+        done();
+      });
+    });
   });
 
 });
